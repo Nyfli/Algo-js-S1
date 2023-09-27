08d23fbfcff74bedbab19d375a096e47cb8b2334
Nyfli <146163418+Nyfli@users.noreply.github.com>
2023-09-27 12:17:40 +0200

    Add files via upload

---
+++ Fonction.js
@@ -0,0 +1,13 @@
+var a = "Jean"
+var b = "Paul"
+var result = checkName(a,b)
+
+function checkName(a,b){
+    if(a===b){
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(result)
