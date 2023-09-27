b8b7f1ca6e477627303e885e4b24a2698e025ba7
Nyfli <146163418+Nyfli@users.noreply.github.com>
2023-09-27 12:20:32 +0200

    Add files via upload

---
+++ Object.js
@@ -0,0 +1,7 @@
+let politique = {
+    lastname : "benalla",
+    FirstName : "alexandre",
+    age : 32
+}
+
+console.log("Voicie ", politique.FirstName, politique.lastname, " il à ", politique.age, " et il a un bon crochet du droit.")
